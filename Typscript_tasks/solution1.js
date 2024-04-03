335a5350a7dcedbb86c75d48c056746624f2c698
abdulaziz0413 <oktava0413@gmail.com>
2024-04-03 12:22:58 +0500

    my_new_angular_project

---
+++ Typscript_tasks/solution1.js
@@ -0,0 +1,4 @@
+var id_name = "Abdulaziz";
+var id_age = 23;
+console.log(id_name);
+console.log(id_age);
---
+++ Typscript_tasks/solution1.ts
@@ -0,0 +1,5 @@
+let id_name: string="Abdulaziz";
+let id_age: number=23;
+
+console.log(id_name);
+console.log(id_age);
