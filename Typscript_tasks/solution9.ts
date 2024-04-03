677204ba3f7d4249e9cb308f97529e3dffe41d7e
abdulaziz0413 <oktava0413@gmail.com>
2024-04-03 16:56:53 +0500

    homewor is ready

---
+++ Typscript_tasks/solution9.ts
@@ -0,0 +1,12 @@
+enum Color {
+    Red,
+    Green,
+    White,
+    Blue,
+  }
+  console.log("Ranglar listi:",Color);
+ 
+  let selectedColor: Color = Color.Green;
+  
+  console.log("Tanlangan rang", selectedColor);
+  
